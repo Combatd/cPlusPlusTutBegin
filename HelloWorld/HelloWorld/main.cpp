6b1edb23085e9ed3f1bb276920a93f0eff85cf95
Combatd <combatdstar@gmail.com>
2021-10-30 20:38:02 -0700

    Test Hello World std::cout

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Mark Calvelo on 10/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello there, world!" << std::endl;
+    return 0;
+}
