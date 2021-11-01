f0d2295f31b078ed4294481fe3dfdcb808865df8
Combatd <combatdstar@gmail.com>
2021-11-01 01:43:31 -0700

    Create Variables Project

---
+++ Variables/Variables/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Variables
+//
+//  Created by Mark Calvelo on 11/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
