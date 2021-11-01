dd131a88bd556cefafafcf6848c7cece3c8cf936
Combatd <combatdstar@gmail.com>
2021-11-01 01:00:51 -0700

    Print out more text using std

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -8,7 +8,13 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    std::cout << "Starting program..." << std::flush; // std::cout is buffered, and the actual output is only printed once the buffer is full or some other flushing situation occurs (e.g. a newline in the stream). Sometimes you want to make sure that the printing happens immediately, and you need to flush manually.
+    
+
     std::cout << "Hello there, world!" << std::endl;
+    std::cout << "This is some text." << std::endl; // carraige return takes a line into a new line
+    std::cout << "Banana." << " Apple." << " Orange." << std::endl;
+    std::cout << "More text! \n";
+    
     return 0;
 }
