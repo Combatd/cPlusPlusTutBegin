74834ef464f28a24111e0707ad86442d923f244d
Combatd <combatdstar@gmail.com>
2021-11-01 02:11:52 -0700

    Create strings project

---
+++ Strings/Strings/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Strings
+//
+//  Created by Mark Calvelo on 11/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::string text1 = "Hello, this is a string.";
+    std::cout << text1 << std::endl;
+    return 0;
+}
