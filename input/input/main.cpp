646ceb2703dbb330f68abb7ee9d3e000c69e2142
Combatd <combatdstar@gmail.com>
2021-11-03 01:38:55 -0700

    Create Input Project

---
+++ input/input/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  input
+//
+//  Created by Mark Calvelo on 11/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
