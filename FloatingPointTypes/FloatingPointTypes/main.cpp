4a89246787d0502f8b9d2e9873a06479db523e2a
Combatd <combatdstar@gmail.com>
2021-11-06 17:33:56 -0700

    Print float

--- FloatingPointTypes/FloatingPointTypes/main.cpp
+++ FloatingPointTypes/FloatingPointTypes/main.cpp
@@ -8,7 +8,9 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    float floatValue = 76.4;
+    
+    std::cout << floatValue << std::endl;
+    
     return 0;
 }
