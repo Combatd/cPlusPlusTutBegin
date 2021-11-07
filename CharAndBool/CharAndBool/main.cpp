559bdb438a3bf5c05e2bc34d86fb3dd6e70ce201
Combatd <combatdstar@gmail.com>
2021-11-07 02:16:15 -0800

    Create CharAndBool

---
+++ CharAndBool/CharAndBool/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CharAndBool
+//
+//  Created by Mark Calvelo on 11/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
