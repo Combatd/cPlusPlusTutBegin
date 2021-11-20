6fdf145ba8f200fb96e68974c6c880494ecd2e0c
Combatd <combatdstar@gmail.com>
2021-11-20 15:35:52 -0800

    Finish simple console program

--- IfElse/IfElse/main.cpp
+++ IfElse/IfElse/main.cpp
@@ -9,11 +9,28 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Add new record \n";
-    std::cout << "Delete record \n";
-    std::cout << "View record \n";
-    std::cout << "Search record \n";
-    std::cout << "Quit \n";
+    std::cout << "1. Add new record \n";
+    std::cout << "2. Delete record \n";
+    std::cout << "3. View record \n";
+    std::cout << "4. Search record \n";
+    std::cout << "5. Quit \n";
+    
+    std::cout << "Enter your selection: " << std::flush;
+    
+    int value;
+    std::cin >> value;
+    
+    if(value < 3) {
+        std::cout << "Insufficient privileges to use this menu option." << std::endl;
+    } else {
+        std::cout << "Privilege level sufficient." << std::endl;
+    }
+    
+    if(value == 5) {
+        std::cout << "Quitting..."
+    } else if(value > 5) {
+        std::cout << "Enter a valid record next time." << std::endl;
+    }
     
     return 0;
 }
