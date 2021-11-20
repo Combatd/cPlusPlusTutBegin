1dca616a4be3550ef45d1efeb3f7e4b566f4d40a
Combatd <combatdstar@gmail.com>
2021-11-20 13:43:08 -0800

    Output text about records

--- IfElse/IfElse/main.cpp
+++ IfElse/IfElse/main.cpp
@@ -9,6 +9,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Add new record \n";
+    std::cout << "Delete record \n";
+    std::cout << "View record \n";
+    std::cout << "Search record \n";
+    std::cout << "Quit \n";
+    
     return 0;
 }
