f07dbef04955c83030a604ec6bd9ec36fa7488d5
Combatd <combatdstar@gmail.com>
2021-11-20 12:47:33 -0800

    Collect user input

--- IfStatements/IfStatements/main.cpp
+++ IfStatements/IfStatements/main.cpp
@@ -8,7 +8,13 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "Enter your password: " << std::flush;
+    
+    std::string input;
+    std::cin >> input;
+    
+    std::cout << "'" << input << "'" << std::endl;
+    
     return 0;
 }
