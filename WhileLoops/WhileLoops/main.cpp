1ead5c37c135610fc17f1d7963ca3ec8d58e0974
Combatd <combatdstar@gmail.com>
2025-09-10 17:41:47 -0700

    Create a do while loop for input

--- WhileLoops/WhileLoops/main.cpp
+++ WhileLoops/WhileLoops/main.cpp
@@ -9,17 +9,42 @@
 
 int main(int argc, const char * argv[]) {
     
-    int i = 0;
+//    int i = 0;
+//    
+//    while (i <= 6) {
+//        std::cout << "Value of i: " << i << std::endl;
+//        
+//        // i = i + 1;
+//        i++;
+//        std::cout << "we incremented i to " << i << " and onto next iteration" << std::endl;
+//    }
+//    
+//    std::cout << "Program qutting..." << std::endl;
     
-    while (i <= 6) {
-        std::cout << "Value of i: " << i << std::endl;
+//    do while loops
+    
+    int value = 0;
+    
+    do {
+        std::cout << "Hello! This will run at least once." << std::endl;
+        value++;
+    } while(value < 5);
+    
+    const std::string password = "hello";
+    std::string input;
+    
+    do {
+       
+        std::cout << "Enter your password: " << std::flush;
+        std::cin >> input;
         
-        // i = i + 1;
-        i++;
-        std::cout << "we incremented i to " << i << " and onto next iteration" << std::endl;
-    }
+        if(input != password) {
+            std::cout << "Access denied." << std::endl;
+        }
+    } while (input != password);
     
-    std::cout << "Program qutting..." << std::endl;
+    std::cout << "Password acceptes" << std::endl;
+    
     
     return 0;
 }
