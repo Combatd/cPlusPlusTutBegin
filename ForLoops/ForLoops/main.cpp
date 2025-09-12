161bcd6fcf47fa16e6f458c22e1a197507f6ed50
Combatd <combatdstar@gmail.com>
2025-09-11 17:20:18 -0700

    Use a for loop for output

---
+++ ForLoops/ForLoops/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ForLoops
+//
+//  Created by Mark Calvelo on 9/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Hello " << i << std::endl;
+    }
+    
+    return 0;
+}
