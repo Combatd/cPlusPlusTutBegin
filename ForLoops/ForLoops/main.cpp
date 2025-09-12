f51cc867f20ae5d3c402f8bd3fb8d8adeb8e37eb
Combatd <combatdstar@gmail.com>
2025-09-11 17:32:00 -0700

    Break loops and continue to skip iterations

--- ForLoops/ForLoops/main.cpp
+++ ForLoops/ForLoops/main.cpp
@@ -13,5 +13,47 @@ int main(int argc, const char * argv[]) {
         std::cout << "Hello " << i << std::endl;
     }
     
+    /*
+     * break
+     */
+    for (int i = 0; i < 5; i++) {
+        std::cout << "i is: " << i << std::endl;
+        
+        if(i == 3) {
+            break; // the rest of the for loop will stopp executing
+        }
+    }
+    
+    /*
+     * continue
+     */
+    for (int i = 0; i < 5; i++) {
+        std::cout << "i is: " << i << std::endl;
+        
+        if(i == 3) {
+            continue; // on this specific iteration, nothing after this line will execute
+        }
+        
+        std::cout << "Continuing..." << std::endl;
+    }
+    
+    std::cout << "Program quitting..." << std::endl;
+    
+    const std::string password = "hello";
+    std::string input;
+
+    do {
+        std::cout << "Enter your password: " << std::flush;
+        std::cin >> input;
+        
+        if (input == password) {
+            break; // terminae
+        } else {
+            std::cout << "Access denied..." << std::endl;
+        }
+        
+    } while (true);
+    
+    
     return 0;
 }
