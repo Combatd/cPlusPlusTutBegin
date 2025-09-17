249c2402d6ac28b6459068f5ac972f50984f7cf7
Combatd <combatdstar@gmail.com>
2025-09-17 14:33:12 -0700

    Create switch cases

--- Switch/Switch/main.cpp
+++ Switch/Switch/main.cpp
@@ -8,7 +8,40 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int value = 5;
+    
+    switch (value) {
+        case 4:
+            std::cout << "Value is 4" << std::endl;
+            break;
+        case 5:
+            std::cout << "Value is 5" << std::endl;
+            break;
+        case 6:
+            std::cout << "Value is 6" << std::endl;
+            break;
+        default:
+            std::cout << "Unrecognized value." << std::endl;
+            break;
+    }
+    
+    char stillAnIntegerType = 6;
+    
+    switch (stillAnIntegerType) {
+        case 4:
+            std::cout << "character is 4" << std::endl;
+            break;
+        case 5:
+            std::cout << "character is 5" << std::endl;
+            break;
+        case 6:
+            std::cout << "character is 6" << std::endl;
+            break;
+        default:
+            std::cout << "Unrecognized value." << std::endl;
+            break;
+    }
+    
     return 0;
 }
