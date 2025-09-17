c48807d85bf3edf9b0f08b84157fceb0ae1711b6
Combatd <combatdstar@gmail.com>
2025-09-17 16:02:06 -0700

    Define #showMenu #processSelection

--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -7,8 +7,42 @@
 
 #include <iostream>
 
+void showMenu() {
+    std::cout << "1. Search" << std::endl;
+    std::cout << "2. View Record" << std::endl;
+    std::cout << "3. Quit" << std::endl;
+}
+
+void processSelection() {
+    int input;
+    std::cin >> input;
+    
+    switch (input) {
+        case 1:
+            std::cout << "Saving.";
+            break;
+        case 2:
+            std::cout << "Viewing..." << std::endl;
+            break;
+        case 3:
+            std::cout << "Quitting..." << std::endl;
+            break;
+        default:
+            std::cout << "Please select an item from the menu." << std::endl;
+            showMenu();
+            std::cout << std::endl;
+            processSelection();
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    /*
+     * A function in C++ is a reusable block of code that can be invoked
+     * and split up your program into logical sections rather than writing
+     * procedural code all over the place
+     */
+    
+    showMenu();
+    processSelection();
     return 0;
 }
