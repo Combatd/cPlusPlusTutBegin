af61f2d7d5b48a91becb4764f81c3cc87ee06874
Combatd <combatdstar@gmail.com>
2025-10-04 16:25:39 -0700

    Create ConstructorsAndDestructors project

---
+++ ConstructorsAndDestructors/ConstructorsAndDestructors/src/Classes.cpp
@@ -0,0 +1,19 @@
+//
+//  Classes.cpp
+//  Classes
+//
+//  Created by Mark Calvelo on 10/4/25.
+//
+
+#include <stdio.h>
+#include "Cat.h"
+
+int main() {
+    
+    Cat cat1;
+    
+    cat1.speak();
+    cat1.jump();
+    
+    return 0;
+}
