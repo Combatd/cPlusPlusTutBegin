ec3bb4024acc03bb4a80fb756d415cca0f8cb2a1
Combatd <combatdstar@gmail.com>
2025-10-04 04:47:33 -0700

    Create Classes project

---
+++ Classes/Classes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Classes
+//
+//  Created by Mark Calvelo on 10/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
