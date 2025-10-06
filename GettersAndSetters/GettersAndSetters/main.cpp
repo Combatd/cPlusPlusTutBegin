baf4517c1f411c9c2b3882f4a00d8e77337cb96b
Combatd <combatdstar@gmail.com>
2025-10-06 03:55:04 -0700

    Create GettersAndSetters project

---
+++ GettersAndSetters/GettersAndSetters/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GettersAndSetters
+//
+//  Created by Mark Calvelo on 10/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
