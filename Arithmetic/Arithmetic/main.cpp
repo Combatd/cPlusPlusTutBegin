ed24a3e2c7c7d9ef4761b6fa0efdfc663406fade
Combatd <combatdstar@gmail.com>
2025-10-12 03:32:54 -0700

    Create Arithmetic project

---
+++ Arithmetic/Arithmetic/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Arithmetic
+//
+//  Created by Mark Calvelo on 10/12/25.
+//
+
+#include <iostream>
+
+/*
+ 
+ +
+ -
+ *
+ /
+ +=
+ -=
+ *=
+ /=
+ %
+ %=
+ precedence
+ 
+ */
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
