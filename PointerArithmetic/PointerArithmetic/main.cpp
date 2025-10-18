cf63bb0e4f36a839c07ca5159d7c8e525488abfa
Combatd <combatdstar@gmail.com>
2025-10-18 11:10:41 -0700

    Set and dereference pointers doing arithmetic

--- PointerArithmetic/PointerArithmetic/main.cpp
+++ PointerArithmetic/PointerArithmetic/main.cpp
@@ -8,7 +8,42 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    const int NSTRINGS = 5;
+    
+    std::string texts[NSTRINGS] = {"one", "two", "three", "four", "five"};
+    
+    std::string *pTexts = texts;
+    
+    pTexts += 3;
+    
+    std::cout << *pTexts << std::endl;
+    
+    pTexts -= 2;
+    
+    std::cout << *pTexts << std::endl;
+    
+    std::string *pEnd = &texts[NSTRINGS - 1];
+    pTexts = &texts[0];
+    
+    while (pTexts != pEnd) {
+        std::cout << *pTexts << std::endl;
+        pTexts++;
+    }
+    
+    // Set pTexts back to start
+    pTexts = &texts[0];
+    
+    long elements = (long)(pEnd - pTexts);
+    
+    std::cout << elements << std::endl;
+    
+    // set pTexts back to start
+    pTexts = &texts[0];
+    
+    pTexts += NSTRINGS / 2;
+    std::cout << *pTexts << std::endl;
+    
+    
     return 0;
 }
