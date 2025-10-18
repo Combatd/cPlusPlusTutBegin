ddb5ec01ffdec057ef897c076a4e8ecd0d015a8c
Combatd <combatdstar@gmail.com>
2025-10-18 12:07:02 -0700

    Loop through character arrays

--- CharArrays/CharArrays/main.cpp
+++ CharArrays/CharArrays/main.cpp
@@ -8,6 +8,28 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-
+    
+    // char text[6] = {'h','e','l','l','o'};
+    char text[] = "hello";
+    
+    std::cout << text << std::endl;
+    
+    for (int i = 0; i < sizeof(text); i++) {
+        std::cout << i << ": " << text[i] << " " << std::flush;
+    }
+    
+    int k = 0;
+    
+    while (true) {
+        
+        if (text[k] == 0) {
+            break;
+        }
+        
+        std::cout << text[k] << std::flush;
+        
+        k++;
+    }
+    
     return 0;
 }
