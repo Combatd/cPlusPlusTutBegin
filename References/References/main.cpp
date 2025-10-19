3ca519208e23e26b8c2a505eb3c1a45d0058a25a
Combatd <combatdstar@gmail.com>
2025-10-19 15:05:43 -0700

    Define #changeValue

--- References/References/main.cpp
+++ References/References/main.cpp
@@ -7,8 +7,29 @@
 
 #include <iostream>
 
+// parameter is actually a reference to an existing variable - the argument passed in
+void changeValue(double &value) {
+    value = 123.4;
+}
+
 int main(int argc, const char * argv[]) {
     
+    /*
+     value2 is just another name for value 1
+     The & creates a reference to access value1, a new int is NOT created
+     They are like aliases to existing variables in memory
+     */
+    
+    int value1 = 8;
+    int &value2 = value1;
+    value2 = 10;
+    
+    std::cout << "Value 1: " << value1 << std::endl;
+    std::cout << "Value 2: " << value2 << std::endl;
+    
+    double value = 4.321;
+    changeValue(value);
+    std::cout << value << std::endl;
     
     return 0;
 }
