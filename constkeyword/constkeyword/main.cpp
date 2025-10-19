568aab458bc67ff642065dc9dcb13ee49bab6cb0
Combatd <combatdstar@gmail.com>
2025-10-19 15:31:38 -0700

    Create class Animal

--- constkeyword/constkeyword/main.cpp
+++ constkeyword/constkeyword/main.cpp
@@ -6,6 +6,16 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Animal {
+private:
+    string name;
+    
+public:
+    void setName(string name) { this->name = name;  }
+    void speak() { cout << "My name is: " << name << endl; }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
