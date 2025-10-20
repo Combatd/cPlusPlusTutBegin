02c6534f3adc2c5de5db0cabefefb0ddd17e8b7f
Combatd <combatdstar@gmail.com>
2025-10-20 15:53:35 -0700

    Create Animal class

--- CopyConstructors/CopyConstructors/main.cpp
+++ CopyConstructors/CopyConstructors/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Animal {
+private:
+    string name;
+    
+public:
+    void setName(string name) { this->name = name;  }
+    void speak() const { cout << "My name is: " << name << endl; }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
