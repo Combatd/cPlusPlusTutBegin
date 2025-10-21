e4cc59b861ac3a01690725c1b4c7f067a27f0b06
Combatd <combatdstar@gmail.com>
2025-10-21 16:26:53 -0700

    Instantiate Animal object with copy constructor

--- CopyConstructors/CopyConstructors/main.cpp
+++ CopyConstructors/CopyConstructors/main.cpp
@@ -13,11 +13,25 @@ private:
     string name;
     
 public:
+    Animal() { cout << "Animal Created" << endl; }
+    Animal(const Animal &other) { name =(other.name); cout << "Animal created by copying" << endl; }
     void setName(string name) { this->name = name;  }
     void speak() const { cout << "My name is: " << name << endl; }
 };
 
 int main(int argc, const char * argv[]) {
-
+    
+    Animal animal1;
+    animal1.setName("Freddy");
+    
+    Animal animal2 = animal1;
+    animal2.setName("Bob");
+    
+    animal1.speak();
+    animal2.speak();
+    
+    Animal animal3(animal1); // copy constructor instantiation
+    animal3.speak(); // output is still "Freddy", but from the animal3 object
+    
     return 0;
 }
