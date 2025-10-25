2903f209e1f3fa78e5361513c2913fa2c07ff402
Combatd <combatdstar@gmail.com>
2025-10-25 14:15:02 -0700

    Allocate memory in arrays and functions

--- ArraysAndFunctions/ArraysAndFunctions/main.cpp
+++ ArraysAndFunctions/ArraysAndFunctions/main.cpp
@@ -7,8 +7,50 @@
 
 #include <iostream>
 
+using namespace std;
+
+void showText(string texts[], const int nElements) {
+    
+    for (int i = 0; i < nElements; i++) {
+        cout << texts[i] << endl;
+    }
+}
+
+void showTextPointerSize(string (&texts)[3])  {
+    
+    for (int i = 0; i < sizeof(texts) / sizeof(string); i++) {
+        cout << texts[i] << endl;
+    }
+}
+
+void getArray(string (&texts)[3]) {
+    // don't return pointers to local variables! string numbers[] = {"1"};
+    for (int i = 0; i < sizeof(texts) / sizeof(string); i++) {
+        cout << texts[i] << endl;
+    }
+}
+
+char * getMemory() {
+    char *pMem = new char[100];
+    
+    return pMem;
+}
+
+void freeMemory(char *pMem) {
+    delete [] pMem;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+    
+    string texts[] = {"apple", "orange", "banana"};
+    
+    cout << sizeof(texts) / sizeof(string) << endl;
+    
+    showText(texts, 3);
+    showTextPointerSize(texts);
+    
+    char *pMemory = getMemory();
+    freeMemory(pMemory);
+    
+    return 0;
 }
