918aaeeb0106be0fef3b3d92135862d2012d2b23
Combatd <combatdstar@gmail.com>
2025-10-26 12:30:18 -0700

    Create class Animal, Cat, Tiger

--- Inheritance/Inheritance/main.cpp
+++ Inheritance/Inheritance/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Animal {
+    
+public:
+    void speak() {
+        cout << "Grrr" << endl;
+    }
+};
+
+class Cat: public Animal {
+public:
+    void jump() {
+        cout << "Cat jumping" << endl;
+    }
+};
+
+class Tiger: public Cat {
+public:
+    void attackPrey() {
+        cout << "Attacking prey!" << endl;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+    
+    Cat cat1;
+    cat1.speak(); // ==> "Grrr"
+    cat1.jump(); // = "Cat jumping"
+    
+    Tiger tiger1;
+    tiger1.speak();
+    tiger1.jump();
+    tiger1.attackPrey(); // => "Attacking prey!"
+    
+    return 0;
 }
